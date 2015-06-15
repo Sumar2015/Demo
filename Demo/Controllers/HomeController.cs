5c8584232ea3e3113c96e1f3a39aa42f0c7cf6ca
thordur13 <thordur13@ru.is>
2015-06-15 13:45:28 +0000

    changes made to about and contact views

--- Demo/Controllers/HomeController.cs
+++ Demo/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Demo.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Information about this final project.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact information.";
 
             return View();
         }
